960c7d2a0e86e001f99466b974fa254c1abc078a
kanocyan <141991013+kanocyann@users.noreply.github.com>
2024-06-20 09:14:13 +0800

    java实验

--- test6/t2/PetImpl.java
+++ test6/t2/PetImpl.java
@@ -3,18 +3,18 @@ package test6.t2;
 public abstract class PetImpl implements Pet {
     private String name;
     private int age;
-    PetImpl(){
+    PetImpl() {
         name="null";
         age=0;
     }
-    PetImpl(String name,int age){
+    PetImpl(String name,int age) {
         this.name=name;
         this.age=age;
     }
-    public String getName(){//接口要求
+    public String getName() {
         return name;
     }
-    public int getAge(){//接口要求
+    public int getAge() {
         return age;
     }
 
@@ -24,9 +24,9 @@ public abstract class PetImpl implements Pet {
     }
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof PetImpl){
+        if(obj instanceof PetImpl) {
             PetImpl pet = (PetImpl)obj;
-            if(pet.getName().equals(getName())&&pet.getAge()==getAge()){
+            if(pet.getName().equals(getName())&&pet.getAge()==getAge()) {
                 return true;
             }
         }
